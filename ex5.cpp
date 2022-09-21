4159847d66ecda9f1936f6ab2c6139e701dbfd00
Дашулька <dashechka.fedorova@mail.ru>
2022-09-21 15:06:22 +0300

    lab5 c++

---
+++ ex5.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+void M(string *M, char H)
+{
+	if (H == '1')
+	{
+		M->substr(5, 13);
+	}
+}
+
+int main()
+{
+	string N = "Семья решила поехать на рыбалку";
+	while (true)
+	{
+		cout << "Выберите операцию:  1) substr, 2) insert, 3) find";
+		char Q; cin >> Q;
+		if (Q == '0') break;
+		M(Q);
+		cout << N;
+
+	}
+}
